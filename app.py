68cff79b84ec149a3b38281260c2ef387072a268
Manu <formycodingwork@gmail.com>
2025-11-19 13:54:57 +0530

    Add Flask web app

---
+++ app.py
@@ -0,0 +1,35 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# Loading model and vectorizer
+with open('xgb_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('dv.pkl', 'rb') as f:
+    dv = pickle.load(f)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    X = dv.transform([customer])
+    
+    prediction = model.predict(X)[0]
+    probability = model.predict_proba(X)[0, 1]
+    
+    return jsonify({
+        "prediction": int(prediction),
+        "probability_of_default": float(probability),
+        "status": "APPROVED" if prediction == 0 else "DEFAULT"
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
